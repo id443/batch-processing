534171924a17b4a76526350bc237548782b37089
Maxim <mksmsmirnov@gmail.com>
2021-07-19 20:39:20 +0300

    Initial commit

---
+++ src/test/java/ru/sma/batchprocessing/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sma.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
